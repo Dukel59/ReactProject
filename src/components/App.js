bb30926c0590c2d290ef2bb5cc08c93553bbfea3
Dukel59 <jonnydukalez@gmail.com>
2021-12-03 22:57:56 +0300

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,133 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "./NavBar";
-import Users from "./Users";
+import trashbox from '../media/trashbox.png';
 
 const App = () => {
+  const [showFormUser, setShowFormUser] = useState(false);
+  const [users, setUsers] = useState([
+    {
+     name:'John',
+     phone: '7788' 
+    },
+    {
+      name:'Thanos',
+      phone: '50%' 
+     },
+  ]);
+  const [user, setUser] = useState(
+    {
+    name:'',
+    phone:'',
+    });
+
+  const onChange = (e) => {
+    if (e.target.id == "name"){
+      setUser({...user, name: e.target.value});
+    } else {
+      setUser({...user, phone: e.target.value});
+    }
+  }
+
+  const addUser = () =>{
+    setUsers([...users,user]);
+    setUser( {
+      name:'',
+      phone:'',
+      });
+  }
+
+  const clearFiels = () =>{
+    setUser({name: "", phone: ""})
+  }
+
+  const deleteUser = (value) =>{
+    setUsers(users.filter((user)=>user.value!=value));
+  }
+
+  console.log(user);
+
   return (
     <div className="App">
       <NavBar/>
-      <Users/>
+      <div className="container">
+        <div className="row m-1">
+          <div className="col s4">
+            <a className="waves-effect waves-light btn"
+              onClick={()=>setShowFormUser(!showFormUser)}
+              >
+                  Add user
+            </a>
+          </div>
+          <div className="col s8">
+           {showFormUser && 
+           <>
+            <div className="input-field col s6">
+              <i className="material-icons prefix">account_circle</i>
+              <input 
+              id="name" 
+              type="text" 
+              class="validate" 
+              value={user.name}
+              placeholder="Enter Name"
+              onChange={onChange}
+              />
+              <a className="waves-effect waves-light btn m-1"
+                onClick={()=>addUser()}>Add</a>
+            </div>
+            <div className="input-field col s6">
+              <i className="material-icons prefix">phone</i>
+              <input 
+              id="phone" 
+              type="tel" 
+              value={user.phone}
+              className="validate"
+              placeholder="Enter Phone"
+              onChange={onChange}
+              />
+              <a className="waves-effect waves-light right btn m-1"
+              onClick ={()=>clearFiels()}>Cancel</a>
+            </div>
+           </> }
+          </div>
+        </div>
+        <table>
+        <thead>
+          <tr>
+              <th>Name</th>
+              <th>Phone</th>
+              <th>Delete</th>
+          </tr>
+        </thead>
+
+        <tbody>
+         { users && users.map((user)=>
+         <tr>
+            <td>{user.name}</td>
+            <td>{user.phone}</td>
+            <td>
+              <a className="deleteLink" href="" onClick={()=>deleteUser(user)}
+              style={{
+                display: "inline-block", 
+                width: "100%", 
+                height: "100%"
+                }}
+                >
+                <img src={trashbox} alt="trashbox" 
+                style={{
+                  height: "30px",
+                  width: "30px"
+                }}
+                />
+              </a>
+            </td>
+          </tr>
+           )}
+        </tbody>
+      </table>
+            
+      </div>
     </div>
   );
-};
+}
 
 export default App;
